30a973d459b14bfc201162e56fcb70549e09bfca
wakim <wakim.jraige@gmail.com>
2017-10-13 11:03:14 -0300

    Using lists to render lists of items on html

--- src/App.js
+++ src/App.js
@@ -2,27 +2,42 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function greetUser(user) {
-  return <p>Hello {user.firstName} {user.lastName}</p>;
-}
-
 class App extends Component {
   render() {
-    const helloWorld = {
-      title: 'Welcome to the Road to learn React',
-      message: 'React is awesome! With Hot Module Reloading!'
-    };
-
-    const user = {
-      firstName: 'Wakim',
-      lastName: 'Jraige'
-    };
+    const list = [
+      {
+        title: 'React',
+        url: 'https://facebook.github.io/react/',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0,
+        order: 0,
+      },
+      {
+        title: 'Redux',
+        url: 'https://github.com/reactjs/redux',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 100,
+        objectID: 1,
+        order: 1,
+      }
+    ];
 
     return (
       <div className="App">
-        <h1>{helloWorld.title}</h1>
-        <h2>{helloWorld.message}</h2>
-        {greetUser(user)}
+        { list.sort((item1, item2) => item1.order - item2.order).map(function(item) {
+          return (<div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </div>);
+          })
+        }
       </div>
     );
   }
