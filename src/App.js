debd91cb079fb5788716be85f62e438f9ae74bc7
wakim <wakim.jraige@gmail.com>
2017-10-15 13:57:21 -0200

    Remove NewArticle component

--- src/App.js
+++ src/App.js
@@ -72,58 +72,6 @@ function Table({list, pattern, onRemove}) {
   );
 }
 
-class NewArticle extends Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      newArticle: { url: '', title: '', author: '' }
-    };
-
-    this.onAdd = this.onSubmit.bind(this);
-    this.onTextChanged = this.onTextChanged.bind(this);
-  }
-
-  onTextChanged(event, attr) {
-    const target = event.target;
-
-    this.setState((prevState, props) => {
-      const newArticle = prevState.newArticle;
-
-      newArticle[attr] = target.value;
-
-      return { newArticle };
-    });
-  }
-
-  onSubmit(e) {
-    e.preventDefault();
-
-    this.setState({newArticle: {url: '', title: '', author: ''}});
-    this.props.onAdd(this.state.newArticle);
-  }
-
-  render() {
-    const {
-      url,
-      title,
-      author,
-    } = this.state.newArticle;
-
-    const { children = 'Add' } = this.props;
-
-    return (
-      <form onSubmit={this.onAdd}>
-        <input type="text" required="required" value={url} onChange={(e) => this.onTextChanged(e, "url")} placeholder="URL" />
-        <input type="text" required="required" value={title} onChange={(e) => this.onTextChanged(e, "title")} placeholder="Title" />
-        <input type="text" required="required" value={author} onChange={(e) => this.onTextChanged(e, "author")} placeholder="Author" />
-        <input type="submit" value={children} className="button" />
-      </form>
-    );
-  }
-}
-
 export default class App extends Component {
 
   constructor(props) {
@@ -132,13 +80,10 @@ export default class App extends Component {
     this.state = {
       list,
       searchTerm: '',
-      nextObjectId: 2,
-      newArticle: { url: '', title: '', author: '' },
     };
 
     this.onSearchChange = this.onSearchChange.bind(this);
     this.onRemove = this.onRemove.bind(this);
-    this.onAdd = this.onAdd.bind(this);
   }
 
   onRemove(objectID) {
@@ -150,23 +95,6 @@ export default class App extends Component {
 
   onSearchChange(searchTerm) {
     this.setState({ searchTerm });
-  }
-
-  onAdd(newArticle) {
-    const {
-      list,
-    } = this.state;
-
-    let nextObjectId = this.state.nextObjectId;
-
-    const newObject = Object.assign({
-      numComments: 0,
-      points: 0,
-      objectID: nextObjectId++,
-      order: list.length,
-    }, newArticle);
-
-    this.setState({list: [...list, newObject], nextObjectId: nextObjectId});
   }
 
   render() {
@@ -186,9 +114,6 @@ export default class App extends Component {
           list={list}
           pattern={searchTerm}
           onRemove={this.onRemove} />
-        <div className="interactions">
-          <NewArticle onAdd={this.onAdd}>Add new article</NewArticle>
-        </div>
       </div>
     );
   }
